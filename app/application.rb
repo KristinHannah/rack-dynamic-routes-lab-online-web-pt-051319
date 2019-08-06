2532bfbb9d2386ccdd369ff7a27d99e1f0f0699c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-08-06 00:13:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     @@items = {}
  
     if req.path.match(/items/)
-      items_title = req.path.split("/items/").last 
+      items_title = req.path.split("/items/").last
       if @@items.include?(items_title)
         resp.write items_title.price
       else 
