190ba2e643fdb55d7c2f826c13b947abfa8dfb71
Kristin Kraemer <kristinhannahk@gmail.com>
2019-08-06 00:12:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,10 @@ class Application
     if req.path.match(/items/)
       items_title = req.path.split("/items/").last 
       if @@items.include?(items_title)
-        resp.write "items_title found"
+        resp.write items_title.price
       else 
-        resp.write "Route not found"
-        resp.status = 404
+        resp.write "item not found"
+        resp.status = 400
       end 
     else 
         resp.write "Route not found"
